146511c09b487afac645f1a0e8b224c6d42fc3ae
CodeSevenic <sevenicbookings@gmail.com>
2022-07-01 15:45:49 +0200

    Ts file update

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLTemplateElement;
 
+  constructor() {
+    this.templateElement = document.getElementById(
+      'project-input'
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById('app')! as HTMLTemplateElement;
+  }
+}
