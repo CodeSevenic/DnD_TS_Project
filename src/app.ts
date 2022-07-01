444eb7004b57a4461712494f3b99617fbd9dc5dd
CodeSevenic <sevenicbookings@gmail.com>
2022-07-01 16:24:49 +0200

    TS file update

--- src/app.ts
+++ src/app.ts
@@ -14,9 +14,12 @@ class ProjectInput {
       true
     );
     this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.attach();
   }
 
   private attach() {
     this.hostElement.insertAdjacentElement('afterbegin', this.element);
   }
 }
+
+const prjInput = new ProjectInput();
