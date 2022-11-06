40976c7fd5d7411eb539b778ad6336abb1bf4e9d
CodeSevenic <sevenicbookings@gmail.com>
2022-11-06 17:05:03 +0200

    Code update

---
+++ src/autobind-decorator.ts
@@ -0,0 +1,15 @@
+﻿namespace App {
+  // autoBind decorator
+  export function autoBind(_: any, _2: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+      configurable: true,
+      get() {
+        const boundFn = originalMethod.bind(this);
+        return boundFn;
+      },
+    };
+
+    return adjDescriptor;
+  }
+}
